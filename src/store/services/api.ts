0f2053944bf2c5518397188dcf97032d533ff2c7
eyobed101 <eyocjjazz101@gmail.com>
2026-07-18 04:42:50 +0300

    api

--- src/store/services/api.ts
+++ src/store/services/api.ts
@@ -849,8 +849,8 @@ export interface UpdateEquipmentStatusPayload {
     notes?: string;
 }
 
-// export const FILE_BASE_URL = 'https://api.arrivalclearance.gov.et';
-export const FILE_BASE_URL = 'http://localhost:3001';
+export const FILE_BASE_URL = 'https://api.arrivalclearance.gov.et';
+// export const FILE_BASE_URL = 'http://localhost:3001';
 // Super Admin Dashboard Types
 export interface SuperAdminMetric {
     value: number;
